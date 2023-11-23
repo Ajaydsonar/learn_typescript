882bd3cfc658a781650f337d0758b52d3eb39228
Ajay <ajaydsoni111@gmail.com>
2023-11-23 12:21:14 +0530

    learn about the tsconfig.json file

---
+++ purest/src/index.ts
@@ -0,0 +1,13 @@
+interface Ajay {
+  name: string;
+  age: number;
+  address: string;
+}
+
+
+
+const ajay: Ajay = { name: "Ajay", age: 25, address: "Pune"  };
+const ajay2: Ajay = { name: "Ajay2", age: 252, address: "Punene"  };
+
+console.log(ajay2);
+console.log(ajay);
